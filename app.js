d3798245cff6dc9217df565aa94315258ef85d9c
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2025-01-19 18:43:57 -0300

    creación e implementacion de la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let input = document.getElementById('amigo');
+  let nombre = input.value.trim();
+  if (nombre.length) {
+    amigos.push(nombre);
+    actualizarLista(amigos)
+  } else {
+    alert('Por favor, inserte un nombre.');
+  };
+  input.value = '';
+} 
